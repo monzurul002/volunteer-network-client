2265c9f8e3deb31a52280fc04ebef51cf8c853bb
monzurul002 <monzurul002@gmail.com>
2023-11-12 23:25:11 +0600

    everythig setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDsbzb8qtX0g79nluwvKJT1jix_iB3jhEs",
+    authDomain: "volunteer-network-2f713.firebaseapp.com",
+    projectId: "volunteer-network-2f713",
+    storageBucket: "volunteer-network-2f713.appspot.com",
+    messagingSenderId: "378246255774",
+    appId: "1:378246255774:web:6c30ff98752bb56c6dd78f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
